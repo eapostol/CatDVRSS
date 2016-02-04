f3ca91620ee7b32c42ccd7286ef930bc091a1fdc
exit <exit>
2016-02-04 15:14:39 -0800

    removed brackets from summary builder
    worker was removing them.

--- libs/response-parser.js
+++ libs/response-parser.js
@@ -3,25 +3,25 @@ var js2xmlparser = require("js2xmlparser");
 
 exports.buildSummary = function( body ){
   var alltext = "<br/>\r\n";
-  alltext += "[Description:] " + body.description + "<br/>\r\n";
-  alltext += "[Source:] " + body.source + "<br/>\r\n";
+  alltext += "-Description: " + body.description + "<br/>\r\n";
+  alltext += "-Source: " + body.source + "<br/>\r\n";
   if(body.source == "Satellite Downlink"){
-    alltext += "-[Satellite:] " + body.satellite + "<br/>\r\n";
-    alltext += "-[Channel:] " + body.channel + "<br/>\r\n";
-    alltext += "-[Polarity:] " + body.polarity + "<br/>\r\n";
-    alltext += "-[FEC:] " + body.fec + "<br/>\r\n";
-    alltext += "-[Symbol Rate:] " + body.symbol + "<br/>\r\n";
-    alltext += "-[Data Rate:] " + body.datarate + "<br/>\r\n";
-    alltext += "-[Window Times:] " + body.window + "<br/>\r\n";
-    alltext += "-[Troubleshooting Number:] " + body.troubleNumber + "<br/>\r\n";
+    alltext += "--Satellite: " + body.satellite + "<br/>\r\n";
+    alltext += "--Channel: " + body.channel + "<br/>\r\n";
+    alltext += "--Polarity: " + body.polarity + "<br/>\r\n";
+    alltext += "--FEC: " + body.fec + "<br/>\r\n";
+    alltext += "--Symbol Rate: " + body.symbol + "<br/>\r\n";
+    alltext += "--Data Rate: " + body.datarate + "<br/>\r\n";
+    alltext += "--Window Times: " + body.window + "<br/>\r\n";
+    alltext += "--Troubleshooting Number: " + body.troubleNumber + "<br/>\r\n";
   }
-  alltext += "[Reporter:] " + body.reporter + "<br/>\r\n";
-  alltext += "[Location:] " + body.location + "<br/>\r\n";
-  alltext += "[Time:] " + body.time + "<br/>\r\n";
-  alltext += "[Timezone:] "+ body.tz + "<br/>\r\n";
-  alltext += "[Format:] " + body.format + "<br/>\r\n";
-  alltext += "[TRT:] " + body.trt + "<br/>\r\n";
-  alltext += "[Embargo:] " + body.embargo + "<br/>\r\n";
+  alltext += "-Reporter: " + body.reporter + "<br/>\r\n";
+  alltext += "-Location: " + body.location + "<br/>\r\n";
+  alltext += "-Time: " + body.time + "<br/>\r\n";
+  alltext += "-Timezone: "+ body.tz + "<br/>\r\n";
+  alltext += "-Format: " + body.format + "<br/>\r\n";
+  alltext += "-TRT: " + body.trt + "<br/>\r\n";
+  alltext += "-Embargo: " + body.embargo + "<br/>\r\n";
   return alltext;
 }
 
@@ -39,7 +39,7 @@ exports.buildXML = function(data){
         USER8: null, // related slugs
         USER9: null, 
         USER10: null, //publish or deliver to
-        NOTES: "bin="+data.feed 
+        NOTES: "bin="+data.feed
       }
   };
   var outXML = js2xmlparser("CLIPS", outData);
