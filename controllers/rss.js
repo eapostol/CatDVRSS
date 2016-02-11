cdd655f4d6d8b40aa98de7c8234a0f86b109e679
exit <exit>
2016-02-11 10:25:50 -0800

    changed to two days
    since most inews is only caching for 3 days need to keep rss less than
    that.

--- controllers/rss.js
+++ controllers/rss.js
@@ -13,10 +13,10 @@ var validUrl = require('valid-url');
 
 var feeds = [
 	{title: "BreakingNews", catID: 1727, display: "Breaking News", newer: 86400 },
-	{title: "Shared", catID: 4102, display: "Shared", newer: 86400 * 3 },
-	{title: "Enterprise", catID: 4130, display: "Enterprise", newer: 86400 * 7 },
-	{title: "TheNow", catID: 3802, display: "The Now", newer: 86400 * 3 },
-	{title: "DCBureau", catID: 5652, display: "DC Bureau", newer: 86400 * 7 }
+	{title: "Shared", catID: 4102, display: "Shared", newer: 86400 * 2 },
+	{title: "Enterprise", catID: 4130, display: "Enterprise", newer: 86400 * 2 },
+	{title: "TheNow", catID: 3802, display: "The Now", newer: 86400 * 2 },
+	{title: "DCBureau", catID: 5652, display: "DC Bureau", newer: 86400 * 2 }
 ]
 
 
