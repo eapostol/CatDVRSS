4322e81c728b0c220a82b3191e96a5cc058f66d7
AndrewMoore10 <Andrew.Moore@10news.com>
2016-06-16 12:11:03 -0700

    commas

--- config/feeds.js
+++ config/feeds.js
@@ -4,17 +4,17 @@ module.exports = [
   {title: "BreakingNews", catID: 1727, display: "Breaking News", newer: 86400 },
   {title: "Enterprise", catID: 4130, display: "Enterprise", newer: 86400 * 2 },
   {title: "TheNow", catID: 3802, display: "The Now", newer: 86400 * 2 },
-  {title: "DCBureau", catID: 5652, display: "DC Bureau", newer: 86400 * 2 }
-  {title: "DWYM", catID: 65757, display: "DWYM", newer: 86400 * 2 }
-  {title: "MAPS", catID: 65777, display: "Maps", newer: 86400 * 2 }
-  {title: "NEWSY", catID: 65754, display: "NEWSY", newer: 86400 * 2 }
-  {title: "SpecialRequests", catID: 65737, display: "Special Requests", newer: 86400 * 2 }
-  {title: "Weather", catID: 65742, display: "Weather", newer: 86400 * 2 }
-  {title: "Wildcard", catID: 65725, display: "Wildcard", newer: 86400 * 2 }
-  {title: "Syndication", catID: 59688, display: "Syndication", newer: 86400 * 2 }
-  {title: "GraphicsBreakingNews", catID: 68000, display: "Graphics BreakingNews", newer: 86400 * 2 }
-  {title: "GraphicsHub", catID: 60202, display: "Graphics Hub", newer: 86400 * 2 }
-  {title: "GraphicsTheNow", catID: 60408, display: "Graphics TheNow", newer: 86400 * 2 }
-  {title: "GraphicsInvestigative", catID: 60429, display: "Graphics Investigative", newer: 86400 * 2 }
+  {title: "DCBureau", catID: 5652, display: "DC Bureau", newer: 86400 * 2 },
+  {title: "DWYM", catID: 65757, display: "DWYM", newer: 86400 * 2 },
+  {title: "MAPS", catID: 65777, display: "Maps", newer: 86400 * 2 },
+  {title: "NEWSY", catID: 65754, display: "NEWSY", newer: 86400 * 2 },
+  {title: "SpecialRequests", catID: 65737, display: "Special Requests", newer: 86400 * 2 },
+  {title: "Weather", catID: 65742, display: "Weather", newer: 86400 * 2 },
+  {title: "Wildcard", catID: 65725, display: "Wildcard", newer: 86400 * 2 },
+  {title: "Syndication", catID: 59688, display: "Syndication", newer: 86400 * 2 },
+  {title: "GraphicsBreakingNews", catID: 68000, display: "Graphics BreakingNews", newer: 86400 * 2 },
+  {title: "GraphicsHub", catID: 60202, display: "Graphics Hub", newer: 86400 * 2 },
+  {title: "GraphicsTheNow", catID: 60408, display: "Graphics TheNow", newer: 86400 * 2 },
+  {title: "GraphicsInvestigative", catID: 60429, display: "Graphics Investigative", newer: 86400 * 2 },
   {title: "GraphicsTemplates", catID: 59765, display: "Graphics Templates", newer: 86400 * 2 }
 ]
